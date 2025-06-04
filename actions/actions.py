40a114dc03cd5e08a69e2473bcb3084aff23ea43
ttakhanh <anhkhanhtran2000@gmail.com>
2025-06-04 13:40:03 +0700

    cap nhat dieu kien fallback

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,11 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
 # This is a simple example for a custom action which utters "Hello World!"
 
@@ -25,3 +30,31 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+# class ActionDefaultFallback(Action):
+#     def name(self) -> Text:
+#         return "action_out_of_scope"
+#
+#     def run(self, dispatcher, tracker, domain):
+#         dispatcher.utter_message(text="I'm not sure I understand what you mean. Could you please rephrase or ask about a SuperWAL service?")
+#         return []
+class ActionOutOfScope(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_out_of_scope"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        fallback_count = tracker.get_slot("fallback_count") or 0
+        fallback_count += 1
+
+        if fallback_count > 2:
+            dispatcher.utter_message("I'm not sure I understand what you mean. Could you please rephrase or ask about a SuperWAL service?")
+            return [SlotSet("fallback_count", 0)]
+        else:
+            dispatcher.utter_message("I'm not sure I understand what you mean. Could you please rephrase or ask about a SuperWAL service?")
+            return [SlotSet("fallback_count", fallback_count)]
